641a93cfc41e1d437f6c1cb4631a0e6ca8cc58c3
Nitin Kumar <nitbit710@gmail.com>
2026-01-27 14:51:54 +0530

    grocery section added

--- src/component/foodCategoryCard.js
+++ src/component/foodCategoryCard.js
@@ -1,6 +1,6 @@
 
 
-export default function FoodCategoriesCard({name,image,link}){
+export default function FoodCategoriesCard({name,image,link,bool_name}){
   const handleClick = () => {
     window.location.href = link;
   };
@@ -13,8 +13,10 @@ className="w-[144px] h-[180px] flex flex-col items-center justify-center">
     <img
     src={image}
     alt={name}
-    className="h-full w-full object-contain"
+    className="h-120px w-full object-contain"
     />
+   {bool_name&&<h6 className="mt-2 text-sm">{name}</h6>}
+    
     
 </button>
 
--- src/component/foodcategory.js
+++ src/component/foodcategory.js
@@ -2,10 +2,12 @@ import FoodCategories from "../data/foodCategories";
 import FoodCategoryCard from "./foodCategoryCard";
 import { CDN_URL } from "../data/constant";
 import { IMAGE_SIZE } from "../data/constant";
+import grocery from "../data/groceries";
 export default function Foodcategory(){
     return (
         <div>
-            <h2 className="flex justify-center text-5xl font-bold py-10">Our Best Food Option</h2>
+            <div>
+            <h2 className="flex justify-center text-4xl font-bold py-8">Our Best Food Option</h2>
             <div className="  grid grid-rows-2 grid-flow-col gap-10 overflow-x-auto"> 
                 {
                     FoodCategories.map(items=>( 
@@ -14,11 +16,31 @@ export default function Foodcategory(){
                         name={items.action?.text}
                         image={`${CDN_URL}${IMAGE_SIZE}${items.imageId}`}
                          link={items.action?.link}
+                         bool_name={false}
                         />
                     )
                 )
                 }
             </div>
+            </div>
+            <div>
+                  <h2 className="flex justify-center text-4xl font-bold py-8">Shop Grocery on InstaMart</h2>
+                   <div className="  grid grid-rows-1 grid-flow-col gap-10 overflow-x-auto"> 
+                            {
+
+                            grocery.map(items=>( 
+                        <FoodCategoryCard
+                        key={items.id}
+                        name={items.action?.text}
+                        image={`${CDN_URL}${IMAGE_SIZE}${items.imageId}`}
+                         link={items.action?.link}
+                         bool_name={true}
+                        />
+                    )
+                )
+                }
+                 </div>
+                </div>
         </div>
 
 
